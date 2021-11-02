f307a6b42192af6d638996ed6cd991d9aacdeba8
spiceydev <spiceydev@gmail.com>
2021-11-02 15:04:05 +1300

    changed to prod build

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiURL: 'https://eshop-backend-spiceydev.herokuapp.com/api/v1'
+    apiURL: 'https://eshop-backend-spiceydev.herokuapp.com/api/v1',
+    stripeKey:
+        'pk_test_51Jm5NrKuKyPuB8tPPJrqAtvMq77wGc3KZ343kHCCx289EnQ45Pn3zUkUR68JmivIIhXRSZ7Sfcu6VPOdIhCmBODv00gVh1v1Su'
 };
